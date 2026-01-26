0561158cdff2fc8e6e75c488541e6380f127b514
MrNoodlez-1227 <pchow747@gmail.com>
2026-01-26 21:10:16 +0100

    Add lic 7 test cases

--- test/MainTest.java
+++ test/MainTest.java
@@ -110,5 +110,111 @@ public class MainTest {
         assertFalse(res);
     }
 
+    /**
+     * Test situation where all points are the same
+     */
+    @Test
+    public void lic7FalseWhenAllPointsSame(){
+        for (int points = 3; points < 10; points++){
+            Point[] pointArray = new Point[points];
+            for (int point = 0; point < points; point++){
+                pointArray[point] = new Point(0, 0);
+            }
+
+            LaunchInterceptorConditionParameters lic7Handler = new LaunchInterceptorConditionParameters(4, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            boolean res = lic7Handler.lic7(pointArray, points);
+            Assert.assertFalse(res);
+        }
+    }
+
+    /**
+     * Test situation when points pair differed by exactly LENGHT1
+     */
+    @Test
+    public void lic7FalseWhenPointsPairEqual(){
+        for (int points = 3; points < 10; points++){
+            Point[] pointArray = new Point[points];
+            for (int point = 0; point < points; point++){
+                pointArray[point] = new Point(point * 2, point * 3);
+            }
+
+            LaunchInterceptorConditionParameters lic7Handler = new LaunchInterceptorConditionParameters((points - 1) * 4, 0, 0, 0, 0, 0, 0, 0, points - 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            boolean res = lic7Handler.lic7(pointArray, points);
+            Assert.assertFalse(res);
+        }
+    }
+
+    /**
+     * Test situation where number of points < 3
+     */
+    @Test
+    public void lic7FalseWhenNotEnoughPoints(){
+        for (int points = 1; points < 3; points++){
+            Point[] pointArray = new Point[points];
+            for (int point = 0; point < points; point++){
+                pointArray[point] = new Point(0, 0);
+            }
+
+            LaunchInterceptorConditionParameters lic7Handler = new LaunchInterceptorConditionParameters(4, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            boolean res = lic7Handler.lic7(pointArray, points);
+            Assert.assertFalse(res);
+        }
+    }
+
+    /**
+     * Test situation where there is one exact pair of points longer than LENGTH1
+     */
+    @Test
+    public void lic7TrueWhenOnePair(){
+        for (int points = 10; points < 20; points++){
+            Point[] pointArray = new Point[points];
+            for (int point = 0; point < points; point++){
+                if (point != (points % 10 + 2) ){
+                    pointArray[point] = new Point(1, 1);
+                }
+                else{
+                    pointArray[point] = new Point(point * 2 + 1, point * 3 + 1);
+                }
+            }
+
+            LaunchInterceptorConditionParameters lic7Handler = new LaunchInterceptorConditionParameters(4, 0, 0, 0, 0, 0, 0, 0, points % 10 + 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            boolean res = lic7Handler.lic7(pointArray, points);
+            Assert.assertTrue(res);
+        }
+    }
+
+    /**
+     * Test situation where all pairs longer than LENGTH1
+     */
+    @Test
+    public void lic7TrueWhenAllPair(){
+        for (int points = 10; points < 20; points++){
+            Point[] pointArray = new Point[points];
+            for (int point = 0; point < points; point++){
+                if (point < (points / 2) ){
+                    pointArray[point] = new Point(1, 1);
+                }
+                else{
+                    pointArray[point] = new Point(point * 2 + 1, point * 3 + 1);
+                }
+            }
+
+            for (int point = 0; point < points; point++){
+                System.out.print(pointArray[point].getX());
+                System.out.print(", ");
+                System.out.print(pointArray[point].getY());
+                System.out.println();
+            }
+
+            LaunchInterceptorConditionParameters lic7Handler = new LaunchInterceptorConditionParameters(4, 0, 0, 0, 0, 0, 0, 0, points / 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            boolean res = lic7Handler.lic7(pointArray, points);
+            Assert.assertTrue(res);
+        }
+    }
     
 }
