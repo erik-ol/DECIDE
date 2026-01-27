c0abb86118028fb31c8a49e525f0bf2ddf119756
joss2002 <josefinenyholm02@gmail.com>
2026-01-27 14:57:59 +0100

    feat(lic):  add validateConsecutivePointsSeparation() method skeleton
    Set up the method for LIC-11 Points separation verifecation.
    
    Includes JavaDoc comment describing the method, including parameters and return type descriptions.
    
    ref: #6

--- src/main/java/LaunchInterceptorConditionParameters.java
+++ src/main/java/LaunchInterceptorConditionParameters.java
@@ -255,38 +255,6 @@ public class LaunchInterceptorConditionParameters
         }
         return false;
     }
-    
-    /**
-     * Verifies if there exists at least one set of three points separated
-     *  exactly C_PTS and D_PTS consecutive intervening points, respectively,
-     *  such that: angle < (PI - EPSILON) or angle > (PI + EPSILON), where
-     *  angle is the angle between the three points. This as a part of the LICs.
-     *
-     * @param planarPoints      - The array of Points to validate
-     * @param planarPointAmount - The amount of planar Points within the planarPoints array
-     *
-     * @return true if the points are separated by C_PTS and D_PTS as well as
-     *         fulfilling the validateAngle() condition, otherwise false.
-     */
-    public boolean validateConsecutivePointsSeparation(int planarPointAmount, Point[] planarPoints)
-    {
-        if((planarPointAmount < 5) || (1 <= C_PTS) || (1 <= D_PTS) || ((C_PTS + D_PTS) <= (planarPointAmount - 3)))
-            return false;
-
-        for(int point = 0; point < planarPointAmount; point += (3 + C_PTS + D_PTS))
-        {
-            Point firstPoint = planarPoints[point];
-            Point vertex     = planarPoints[point + C_PTS + 1];
-            Point lastPoint  = planarPoints[point + C_PTS + 1 + D_PTS + 1];
-
-            Point[] cornerPoints = {firstPoint, vertex, lastPoint};
-
-            if(validateAngle(cornerPoints.length, cornerPoints))
-                return true;
-        }
-
-        return false;
-    }
 
     /**
      * Launch Interceptor Condition 7:
@@ -394,6 +362,23 @@ public class LaunchInterceptorConditionParameters
                 return true;
         }
 
+        return false;
+    }
+
+    /**
+     * Verifies if there exist at least one set of two data Points,
+     * separated by exactly G_PTS such that the x-coordinate of the
+     * second Point subtracted with the x-coordinate of the first
+     * Point is lesser than zero. This as a part of the LICs.
+     *
+     * @param planarPoints      - The array of Points to validate
+     * @param planarPointAmount - The amount of planar Points within the planarPoints array
+     *
+     * @return true if two points separated by G_PTS exists and their
+     *         difference is lesser than zero, otherwise false.
+     */
+    public boolean validateConsecutivePointsSeparation(int planarPointAmount, Point[] planarPoints)
+    {
         return false;
     }
 
